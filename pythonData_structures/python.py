9c065768744224cfacba9d4780940d2073c07335
Andisiwe-tech <kuneneandisiwe0@gmail.com>
2025-06-24 09:00:54 +0000

    operators and assignments operators

---
+++ pythonData_structures/python.py
@@ -0,0 +1,28 @@
+
+# Operators
+# addition(+)
+# subtraction(-)
+# multiplication(*)
+# division(/)
+# modulus(%)
+# exponents(**)
+
+x = 10
+y = 3
+
+print(x+y)
+print(x-y)
+print(x/y)
+print(x*y)
+print(x%y)
+print(x**y)
+
+# assignment operators
+
+x = 10
+x += 2
+x -= 2
+x %= 3
+x *= 2
+print(x)
+
