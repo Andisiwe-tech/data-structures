c9ee9f101909d263e660592fe1e859f4b9c12fe8
Andisiwe-tech <kuneneandisiwe0@gmail.com>
2025-06-24 08:50:15 +0000

    dictionary keys and values

---
+++ pythonData_structures/dictionary.py
@@ -0,0 +1,13 @@
+
+#dictionary
+
+my_dict = {'apple': 3, 'banana': 5, 'orange': 2}
+print(my_dict['apple'])
+
+my_dict['banana'] = 6
+print(my_dict)    #changes the banana key value to 6
+
+my_dict['grape'] = 8
+print(my_dict)      #adds the grape key, with its value of 8
+
+
